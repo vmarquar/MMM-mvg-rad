2d20c747e242bc22bce7387c01264900acfd927e
vmarquar <Valentin.Marquart@gmail.com>
2018-12-23 16:18:16 +0100

    v0-0-1 Cleaned and updated repo

--- MMM-mvg-rad.js
+++ MMM-mvg-rad.js
@@ -1,296 +1,284 @@
 Module.register("MMM-mvg-rad", {
-    defaults: {
-      MAX_ITEMS: 5,
-      refresh_rate : 3*60*1000, //milliseconds
-      stationName: "Pickelstr. 2, München",
-      center: [11.53468800, 48.16904000],
-      radius: 200,
-      bearingCorrection: 0, // 0 is north, you can enter a correction to adapt the direction arrows from your mirror's "point of view"
-      returnType:"return_geojson", //"return_geojson", // "return_table"
-      mapboxAPI: "", 
-      mapboxStyle: 'mapbox://styles/mapbox/dark-v9',
-      text_color: "#FFFFFF"
-    },
-    start: function (){
-      
-      this.bike_table_data = []
-      this._map = null
-      if(this.config.returnType == "return_table"){
-        this.renderTable = true
-      } else {
-        this.renderTable = false
-        this.map_element = this.createMapRow()
-      }
-      const CONFIG = {}
-      CONFIG.radius = this.config.radius
-      CONFIG.center = this.config.center
-      CONFIG.returnType = this.config.returnType
-      this.sendSocketNotification("GET_BIKE_DATA", CONFIG)
-      //this.updateDom()
-    },
-    getStyles: function () {
-      return ["MMM-mvg-rad.css",
-        "font-awesome.css",
-        'https://api.tiles.mapbox.com/mapbox-gl-js/v0.52.0/mapbox-gl.css',
-        'https://api.tiles.mapbox.com/mapbox-gl-js/v0.52.0/mapbox-gl.js'
-      ];
+  defaults: {
+    MAX_ITEMS: 5,
+    refresh_rate: 3 * 60 * 1000, //milliseconds
+    stationName: "",
+    center: [11.53468800, 48.16904000],
+    radius: 200,
+    bearingCorrection: 0, // 0 is north, you can enter a correction to adapt the direction arrows from your mirror's "point of view"
+    returnType: "return_geojson", //"return_geojson", // "return_table"
+    mapboxAPI: "",
+    mapboxStyle: 'mapbox://styles/mapbox/dark-v9',
+    text_color: "#FFFFFF"
   },
-    
-    getDom: function() {
-      
-      var wrapper = document.createElement("div");
-      var header = document.createElement("header");
-      if (this.config.stationName !== "nextbike") {
-        header.innerHTML = this.config.stationName;
-      } else {
-        header.innerHTML = this.config.stationName;
-      }
-      wrapper.appendChild(header);
-      var table = document.createElement("table");
-			table.classList.add("small", "table");
-			table.border='0';
-			table.appendChild(this.createSpacerRow());
-			table.appendChild(this.createAmountRow());
-      table.appendChild(this.createSpacerRow());
-      
-      // Table Version
-      if(this.renderTable){
-        if (this.bike_table_data.length > 0){
+  start: function () {
 
-          // List all bikes (MAX ITEMS is larger than the number of bikes)
-          if (this.config.MAX_ITEMS >= this.bike_table_data.length){
-            for (let index = 0; index < this.bike_table_data.length; index++) {
-              const bike_obj = this.bike_table_data[index];
-              table.appendChild(this.createDataRow(bike_obj))
-            }
+    this.bike_table_data = []
+    this._map = null
+    if (this.config.returnType == "return_table") {
+      this.renderTable = true
+    } else {
+      this.renderTable = false
+      this.map_element = this.createMapRow()
+    }
+    const CONFIG = {}
+    CONFIG.radius = this.config.radius
+    CONFIG.center = this.config.center
+    CONFIG.returnType = this.config.returnType
+    this.sendSocketNotification("GET_BIKE_DATA", CONFIG)
+
+  },
+  getStyles: function () {
+    return ['MMM-mvg-rad.css',
+      'font-awesome.css',
+      'mapbox-gl-v_0-52-0.css',
+      'mapbox-gl-v_0-52-0.js'
+    ];
+  },
+
+  getDom: function () {
+
+    var wrapper = document.createElement("div");
+    var header = document.createElement("header");
+    if (this.config.stationName !== "MVG Rad") {
+      header.innerHTML = this.config.stationName;
+    } else {
+      header.innerHTML = this.config.stationName;
+    }
+    wrapper.appendChild(header);
+    var table = document.createElement("table");
+    table.classList.add("small", "table");
+    table.border = '0';
+    table.appendChild(this.createSpacerRow());
+    table.appendChild(this.createAmountRow());
+    table.appendChild(this.createSpacerRow());
+
+    // Table Version
+    if (this.renderTable) {
+      if (this.bike_table_data.length > 0) {
+
+        // List all bikes (MAX ITEMS is larger than the number of bikes)
+        if (this.config.MAX_ITEMS >= this.bike_table_data.length) {
+          for (let index = 0; index < this.bike_table_data.length; index++) {
+            const bike_obj = this.bike_table_data[index];
+            table.appendChild(this.createDataRow(bike_obj))
+          }
           // List only the neareast bikes until MAX ITEMS is reached
-          } else {
-            for (let index = 0; index < this.config.MAX_ITEMS; index++) {
-              const bike_obj = this.bike_table_data[index];
-              table.appendChild(this.createDataRow(bike_obj))
-            }
+        } else {
+          for (let index = 0; index < this.config.MAX_ITEMS; index++) {
+            const bike_obj = this.bike_table_data[index];
+            table.appendChild(this.createDataRow(bike_obj))
           }
         }
-        wrapper.appendChild(table)
+      }
+      wrapper.appendChild(table)
 
       // Map Version
-      } else if (!this.renderTable){
-        console.log(this.bike_table_data)
-        wrapper.appendChild(this.map_element)
-      }
-      
-      return wrapper
-    },
-    
-    socketNotificationReceived: function (notification, payload) {
-      switch (notification) {
-        case "BIKE_DATA_FETCHED":
-          // add geojson or table data to variable
-          this.bike_table_data = payload
-          
-          // add bikes to map
-          if (!this.renderTable){
-            this.addBikesToMap(this.bike_table_data)
-          }
-          this.updateDom()
-          break
-      }
-    },
-    
-    createSpacerRow: function () {
-      var spacerRow = document.createElement("tr");
-
-      var spacerHeader = document.createElement("td");
-      spacerHeader.className = "spacerRow";
-      spacerHeader.setAttribute("colSpan", "2");
-      spacerHeader.innerHTML = "";
-      spacerRow.appendChild(spacerHeader);
-
-      return spacerRow;
-    },
-
-    createAmountRow: function () {
-      var amountRow = document.createElement("tr");
-
-      var amount = document.createElement("td");
-      amount.className = "amountRow";
-      amount.setAttribute("colSpan", "42");
-      if (!this.bike_table_data || (this.bike_table_data.length) < 1) {
-        amount.innerHTML = "Keine Fahrräder verfügbar!";
-      } else {
-        if (this.config.MAX_ITEMS >= this.bike_table_data.length){
-          amount.innerHTML = "Fahrräder verfügbar:" + " " + (this.bike_table_data.length)
-        } else {
-          amount.innerHTML = "Fahrräder verfügbar:" + " +" + (this.config.MAX_ITEMS)
-        }
-      }
-      amountRow.appendChild(amount)
-
-      return amountRow;
-    },
-    createDataRow: function (bike_obj) {
-      var row = document.createElement("tr");
-
-      // Bike icon
-      var symbol = document.createElement("td");
-      symbol.setAttribute("width", "12px");
-      symbol.className = "fa fa-bicycle";
-      row.appendChild(symbol);
-
-      // Bike Name / Number
-      var bikeNo = document.createElement("td");
-      bikeNo.className = "bikeNo";
-      bikeNo.setAttribute("width", "60px")
-      bikeNo.innerHTML = bike_obj.bikeNumber
-      row.appendChild(bikeNo);
-
-      // Distance to bike from center point
-      var bikeDist = document.createElement("td");
-      bikeDist.className = "bikeNo";
-      bikeDist.setAttribute("width", "40px")
-      bikeDist.innerHTML = String(Math.round(bike_obj.distance))+' m'
-      row.appendChild(bikeDist);
-
-      var symbol_arrow = document.createElement("td");
-      symbol_arrow.setAttribute("width", "12px");
-      symbol_arrow.className = "fa fa-location-arrow";
-      var bearing_int = Math.round(bike_obj.bearing)
-      symbol_arrow.setAttribute('style', symbol_arrow.getAttribute('style')+'; transform: rotate('+String(bearing_int-45+this.config.bearingCorrection)+'deg)')
-      row.appendChild(symbol_arrow);
-
-
-      return row;
-    },
-    createMapRow: function () {
-      
-      var map_elem = document.createElement("div")
-      map_elem.id = "map"
-      // wrapper.appendChild(map_elem)
-      mapboxgl.accessToken = this.config.mapboxAPI
-      this._map = new mapboxgl.Map({
-        container: map_elem,
-        style: this.config.mapboxStyle,
-        center: this.config.center,
-        zoom: 14.00
-      });
-      
-      
-      var _this = this
-      _this._map.on('style.load', function () {
-        _this._map.addSource('center_source', {
-          type: 'geojson',
-          data: {"type": "Point","coordinates": _this.config.center}
-        });
-
-        _this._map.addLayer({
-          "id": "point",
-          "source": "center_source",
-          "type": "circle",
-          "paint": {
-            "circle-radius": 4,
-            "circle-color": "#007cbf"
-          }
-        });
-      })
-      
-      return(map_elem)
-    },
-    addBikesToMap: function (geojson) {
-      var _this = this
-      
-      if (_this._map.getSource('bikes_source') == null) {
-
-        _this._map.on('load', function () {
-
-          console.log("GEOJSON IST:")
-          console.log(geojson)
-          _this._map.addSource('bikes_source', {
-            type: 'geojson',
-            cluster: true,
-            clusterMaxZoom: 16, // Max zoom to cluster points on
-            clusterRadius: 15, // Radius of each cluster when clustering points (defaults to 50)
-            data: geojson
-          });
-    
-        _this._map.addLayer({
-            id: "bikes-count",
-            type: "symbol",
-            source: "bikes_source",
-            filter: ["has", "point_count"],
-            layout: {
-                "icon-image": "bicycle-15",
-                "text-field": "{point_count_abbreviated}+",
-                "text-font": ["Open Sans Semibold", "Arial Unicode MS Bold"],
-                "text-offset": [0.9, 0],
-                "text-anchor": "left",
-                "text-size":10,              
-                "text-allow-overlap": true,
-                "icon-allow-overlap": true,
-              },
-              "paint": {
-                "text-color":_this.config.text_color
-              }
-        });
-
-          _this._map.addLayer({
-            "id": "bikes",
-            "type": "symbol",
-            "source": "bikes_source",
-            "filter": ["!", ["has", "point_count"]],
-            "layout": {
-              "icon-image": "bicycle-15",
-              "text-field": "{bikeNumber}",
-              "text-font": ["Open Sans Semibold", "Arial Unicode MS Bold"],
-              "text-offset": [0.9, 0],
-              "text-anchor": "left",
-              "text-size":11,
-              "text-allow-overlap": true,
-              "icon-allow-overlap": true,
-            },
-            "paint": {
-              "text-color":_this.config.text_color
-            }
-          });
-
-          // Original with overlap=true
-          // _this._map.addLayer({
-          //   "id": "bikes",
-          //   "type": "symbol",
-          //   "source": "bikes_source",
-          //   "layout": {
-          //     "icon-image": "bicycle-15",
-          //     "icon-allow-overlap": true,
-          //     "text-field": "{bikeNumber}",
-          //     "text-font": ["Open Sans Semibold", "Arial Unicode MS Bold"],
-          //     "text-offset": [0, 0.6],
-          //     "text-anchor": "top",
-          //     "text-allow-overlap": true,
-          //     "text-size":12
-          //   }
-          // });
-
-          console.log("MMM-mvg-rad: ADDED GEOJSON SOURCE")
-          _this._map.resize()
-          var bounds = new mapboxgl.LngLatBounds()
-          geojson.features.forEach(function(feature) {
-              bounds.extend(feature.geometry.coordinates)
-          })
-          bounds.extend(_this.config.center)
-          _this._map.fitBounds(bounds, {padding:45})
-        })
-
-      } else if (_this._map.getSource('bikes_source') != null) {
-        _this._map.getSource("bikes_source").setData(geojson)
-        _this._map.resize();
-        var bounds = new mapboxgl.LngLatBounds()
-          geojson.features.forEach(function(feature) {
-              bounds.extend(feature.geometry.coordinates)
-          })
-          bounds.extend(_this.config.center)
-          _this._map.fitBounds(bounds, {padding:45})
-        console.log("MMM-mvg-rad: UPDATED GEOJSON SOURCE")
-      }
-
+    } else if (!this.renderTable) {
+      console.log(this.bike_table_data)
+      wrapper.appendChild(this.map_element)
     }
 
-   })
+    return wrapper
+  },
+
+  socketNotificationReceived: function (notification, payload) {
+    switch (notification) {
+      case "BIKE_DATA_FETCHED":
+        // add geojson or table data to variable
+        this.bike_table_data = payload
+
+        // add bikes to map
+        if (!this.renderTable) {
+          this.addBikesToMap(this.bike_table_data)
+        }
+        this.updateDom()
+        break
+    }
+  },
+
+  createSpacerRow: function () {
+    var spacerRow = document.createElement("tr");
+
+    var spacerHeader = document.createElement("td");
+    spacerHeader.className = "spacerRow";
+    spacerHeader.setAttribute("colSpan", "2");
+    spacerHeader.innerHTML = "";
+    spacerRow.appendChild(spacerHeader);
+
+    return spacerRow;
+  },
+
+  createAmountRow: function () {
+    var amountRow = document.createElement("tr");
+
+    var amount = document.createElement("td");
+    amount.className = "amountRow";
+    amount.setAttribute("colSpan", "42");
+    if (!this.bike_table_data || (this.bike_table_data.length) < 1) {
+      amount.innerHTML = "Keine Fahrräder verfügbar!";
+    } else {
+      if (this.config.MAX_ITEMS >= this.bike_table_data.length) {
+        amount.innerHTML = "Fahrräder verfügbar:" + " " + (this.bike_table_data.length)
+      } else {
+        amount.innerHTML = "Fahrräder verfügbar:" + " +" + (this.config.MAX_ITEMS)
+      }
+    }
+    amountRow.appendChild(amount)
+
+    return amountRow;
+  },
+  createDataRow: function (bike_obj) {
+    var row = document.createElement("tr");
+
+    // Bike icon
+    var symbol = document.createElement("td");
+    symbol.setAttribute("width", "12px");
+    symbol.className = "fa fa-bicycle";
+    row.appendChild(symbol);
+
+    // Bike Name / Number
+    var bikeNo = document.createElement("td");
+    bikeNo.className = "bikeNo";
+    bikeNo.setAttribute("width", "60px")
+    bikeNo.innerHTML = bike_obj.bikeNumber
+    row.appendChild(bikeNo);
+
+    // Distance to bike from center point
+    var bikeDist = document.createElement("td");
+    bikeDist.className = "bikeNo";
+    bikeDist.setAttribute("width", "40px")
+    bikeDist.innerHTML = String(Math.round(bike_obj.distance)) + ' m'
+    row.appendChild(bikeDist);
+
+    var symbol_arrow = document.createElement("td");
+    symbol_arrow.setAttribute("width", "12px");
+    symbol_arrow.className = "fa fa-location-arrow";
+    var bearing_int = Math.round(bike_obj.bearing)
+    symbol_arrow.setAttribute('style', symbol_arrow.getAttribute('style') + '; transform: rotate(' + String(bearing_int - 45 + this.config.bearingCorrection) + 'deg)')
+    row.appendChild(symbol_arrow);
+
+
+    return row;
+  },
+  createMapRow: function () {
+
+    var map_elem = document.createElement("div")
+    map_elem.id = "map"
+    // wrapper.appendChild(map_elem)
+    mapboxgl.accessToken = this.config.mapboxAPI
+    this._map = new mapboxgl.Map({
+      container: map_elem,
+      style: this.config.mapboxStyle,
+      center: this.config.center,
+      zoom: 14.00
+    });
+
+
+    var _this = this
+    _this._map.on('style.load', function () {
+      _this._map.addSource('center_source', {
+        type: 'geojson',
+        data: {
+          "type": "Point",
+          "coordinates": _this.config.center
+        }
+      });
+
+      _this._map.addLayer({
+        "id": "point",
+        "source": "center_source",
+        "type": "circle",
+        "paint": {
+          "circle-radius": 4,
+          "circle-color": "#007cbf"
+        }
+      });
+    })
+
+    return (map_elem)
+  },
+  addBikesToMap: function (geojson) {
+    var _this = this
+
+    if (_this._map.getSource('bikes_source') == null) {
+
+      _this._map.on('load', function () {
+
+        console.log("GEOJSON IST:")
+        console.log(geojson)
+        _this._map.addSource('bikes_source', {
+          type: 'geojson',
+          cluster: true,
+          clusterMaxZoom: 16, // Max zoom to cluster points on
+          clusterRadius: 15, // Radius of each cluster when clustering points (defaults to 50)
+          data: geojson
+        });
+
+        _this._map.addLayer({
+          id: "bikes-count",
+          type: "symbol",
+          source: "bikes_source",
+          filter: ["has", "point_count"],
+          layout: {
+            "icon-image": "bicycle-15",
+            "text-field": "{point_count_abbreviated}+",
+            "text-font": ["Open Sans Semibold", "Arial Unicode MS Bold"],
+            "text-offset": [0.9, 0],
+            "text-anchor": "left",
+            "text-size": 10,
+            "text-allow-overlap": true,
+            "icon-allow-overlap": true,
+          },
+          "paint": {
+            "text-color": _this.config.text_color
+          }
+        });
+
+        _this._map.addLayer({
+          "id": "bikes",
+          "type": "symbol",
+          "source": "bikes_source",
+          "filter": ["!", ["has", "point_count"]],
+          "layout": {
+            "icon-image": "bicycle-15",
+            "text-field": "{bikeNumber}",
+            "text-font": ["Open Sans Semibold", "Arial Unicode MS Bold"],
+            "text-offset": [0.9, 0],
+            "text-anchor": "left",
+            "text-size": 11,
+            "text-allow-overlap": true,
+            "icon-allow-overlap": true,
+          },
+          "paint": {
+            "text-color": _this.config.text_color
+          }
+        });
+
+        console.log("MMM-mvg-rad: ADDED GEOJSON SOURCE")
+        _this._map.resize()
+        var bounds = new mapboxgl.LngLatBounds()
+        geojson.features.forEach(function (feature) {
+          bounds.extend(feature.geometry.coordinates)
+        })
+        bounds.extend(_this.config.center)
+        _this._map.fitBounds(bounds, {
+          padding: 45
+        })
+      })
+
+    } else if (_this._map.getSource('bikes_source') != null) {
+      _this._map.getSource("bikes_source").setData(geojson)
+      _this._map.resize();
+      var bounds = new mapboxgl.LngLatBounds()
+      geojson.features.forEach(function (feature) {
+        bounds.extend(feature.geometry.coordinates)
+      })
+      bounds.extend(_this.config.center)
+      _this._map.fitBounds(bounds, {
+        padding: 45
+      })
+      console.log("MMM-mvg-rad: UPDATED GEOJSON SOURCE")
+    }
+  }
+})
--- node_helper.js
+++ node_helper.js
@@ -1,32 +1,27 @@
 var NodeHelper = require("node_helper")
-const fs = require('fs')
 const turf = require('@turf/turf')
 var request = require("request")
 
 module.exports = NodeHelper.create({
-    start: function () {
-        this.countDown = 10000000
-    },
+    start: function () {},
     socketNotificationReceived: function (notification, payload) {
         switch (notification) {
             case "DO_YOUR_JOB":
                 this.sendSocketNotification("I_DID", (this.countDown - payload))
                 break
             case "GET_BIKE_DATA":
-                (async() => {
+                (async () => {
                     var data = await this.fetchBikeData(payload)
                     this.sendSocketNotification("BIKE_DATA_FETCHED", data)
-                    
+
                 })()
                 break
-                // this.sendSocketNotification("BIKE_DATA_FETCHED", this.fetchBikeData(payload))
-                
+
         }
     },
     fetchBikeData: async function (CONFIG) {
-        console.log(`FetchBikeData Function will look around center ${CONFIG.center} at a circular distance of ${CONFIG.radius}.`)
         const RADIUS = CONFIG.radius // 200 m 
-        const CENTER = CONFIG.center // [11.534383, 48.168992]
+        const CENTER = CONFIG.center // [11, 48.5]
         const RETURN_TYPE = CONFIG.returnType // either "return_table" or "return_geosjon"
         /// Function Definitions
         // Download API Data
@@ -46,7 +41,7 @@ module.exports = NodeHelper.create({
         }
         // convert to geojson
         var convertRawToGeojsonFeature = function (input_obj, geojson_featureCollection) {
-            
+
             var geojson_feature = {
                 "type": "Feature",
                 "properties": {
@@ -93,26 +88,18 @@ module.exports = NodeHelper.create({
                 geojson_featureCollection = convertRawToGeojsonFeature(input_obj, geojson_featureCollection)
             }
             var center = turf.point(CENTER)
-            // var bounding_polygon = turf.polygon([
-            //     [
-            //         [11.528886544276745, 48.172082151900256],
-            //         [11.529031651398498, 48.164618121814676],
-            //         [11.544884604450031, 48.164545532720425],
-            //         [11.544413006304334, 48.172118441126962],
-            //         [11.528886544276745, 48.172082151900256]
-            //     ]
-            // ]);
-            var radius = RADIUS/1000;
-            var options = {steps: 30, units: 'kilometers', properties: {foo: 'bar'}};
+            var radius = RADIUS / 1000;
+            var options = {
+                steps: 30,
+                units: 'kilometers',
+                properties: {
+                    foo: 'bar'
+                }
+            };
             var circle = turf.circle(center, radius, options);
 
             var fc = turf.featureCollection(geojson_featureCollection.features)
-
-
-            // var ptsWithin = turf.pointsWithinPolygon(fc, bounding_polygon); // rectangle bounding polyfon
-
             var ptsWithin = turf.pointsWithinPolygon(fc, circle);
-            //fs.writeFile('./mvg_rad_geojson.json', JSON.stringify(geojson_featureCollection, null, 2) , 'utf-8');
 
             var options = {
                 units: 'kilometers'
@@ -125,7 +112,6 @@ module.exports = NodeHelper.create({
                 feature.properties.bearing = bearing
             });
 
-            //ptsWithin.features.sort((a, b) => a.properties.distance.localeCompare(b.properties.distance));
             ptsWithin.features.sort(function (a, b) {
                 return a.properties.distance == b.properties.distance ? 0 : +(a.properties.distance > b.properties.distance) || -1;
             });
@@ -133,25 +119,22 @@ module.exports = NodeHelper.create({
             var near_bikes_table = []
             for (let index = 0; index < ptsWithin.features.length; index++) {
                 const feature = ptsWithin.features[index];
-                console.log(`Fahrrad: ${feature.properties.bikeNumber} Entfernung: ${feature.properties.distance} m Richtung: ${feature.properties.bearing}°`) // output in meter
                 near_bikes_table.push({
                     "bikeNumber": feature.properties.bikeNumber,
                     "distance": feature.properties.distance,
                     "bearing": feature.properties.bearing
                 })
             }
-            if(returnType=="return_table"){
+            if (returnType == "return_table") {
                 return (near_bikes_table)
-            } else if (returnType == "return_geojson"){
+            } else if (returnType == "return_geojson") {
                 return (ptsWithin)
             }
-            
 
         }
-    
+
         var near_bikes = await fetchMVGandProcess(RETURN_TYPE);
-        console.log(near_bikes)
-        return(near_bikes)
+        return (near_bikes)
 
     }
 })
